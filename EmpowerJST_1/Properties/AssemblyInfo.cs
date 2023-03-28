90374394f9e4b0467e8eb2cbaf927ebc1abfa9f4
xknapereks-dataminer <128934587+xknapereks-dataminer@users.noreply.github.com>
2023-03-28 14:24:48 +0200

    Add project files.

---
+++ EmpowerJST_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerJST_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerJST_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7A1AF3C7-293A-4BFC-865D-2F9300F95320")]
+[assembly: AssemblyVersion("1.0.0.0")]
